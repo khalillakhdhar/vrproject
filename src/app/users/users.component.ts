fa6a3e84ac1aceac4b821203ecf19d71d7d73cfa
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-08 10:09:14 +0100

    Update users.component.ts

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../classes/user';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
-
+  users:User[];
+  id:string;
+  constructor(private userService:UserService) { }
+  
   ngOnInit(): void {
+    this.users=[];
+this.id=localStorage.getItem("id");
+    this.read();
+    
   }
+  read()
+  {
+    this.userService.read_Users().subscribe(data => {
+  
+      this.users = data.map(e => {
+        return {
+          id: e.payload.doc.id,
 
+          nom: e.payload.doc.data()["nom"],
+          prenom: e.payload.doc.data()["prenom"],
+          age: e.payload.doc.data()["age"],
+          email: e.payload.doc.data()["email"],
+          mdp: e.payload.doc.data()["mdp"],
+          adresse: e.payload.doc.data()["adresse"],
+          classe: e.payload.doc.data()["classe"],
+          specialite: e.payload.doc.data()["specialite"],
+          grade: e.payload.doc.data()["grade"],
+   
+   
+  
+  
+        };
+      });
+      console.log(this.users);
+    });
+  
+  
+  }
 }
