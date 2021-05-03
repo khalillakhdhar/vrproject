1a581fc6109f6fcbec1c3f75e703e8e87c2b0a88
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-03 12:31:30 +0100

    Update teste.component.ts

--- src/app/teste/teste.component.ts
+++ src/app/teste/teste.component.ts
@@ -9,10 +9,40 @@ import { Streaming } from '../classes/streaming';
 })
 export class TesteComponent implements OnInit {
 streaming:Streaming;
+streamings:Streaming[];
   constructor(private api:StreamingService) { }
 
 
   ngOnInit(): void {
+    this.streaming=new Streaming();
+    this.read();
   }
+add()
+{
+  let strm=Object.assign({},this.streaming);
+  this.api.create_NewStreaming(strm);
+}
+read()
+{
+  this.api.read_Streamings().subscribe(data => {
 
+    this.streamings = data.map(e => {
+      return {
+        id: e.payload.doc.id,
+
+        titre: e.payload.doc.data()["titre"],
+        date: e.payload.doc.data()["date"],
+        classe: e.payload.doc.data()["classe"],
+      
+ 
+ 
+
+
+      };
+    });
+    console.log(this.streamings);
+  });
+
+
+}
 }
